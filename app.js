319c0bdd521180cd69ab07b72a4496898e106748
omarodc <2490065+omarodc@users.noreply.github.com>
2024-03-08 12:39:56 -0600

    finish sign-up-form

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
-const match = document.getElementById("error-match");
-const password = document.getElementById("password");
-const confirmPassword = document.getElementById("confirm-password");
+document.addEventListener("DOMContentLoaded", function () {
+  let password = document.querySelector("#password").value;
+  let confirmPassword = document.querySelector("#confirmPassword").value;
 
-function passwordMatch() {
-  if (password.value == "" && confirmPassword.value == "") {
-    match.style.color = "red";
-    match.textContent =  "* Passwords do not match";
-  } else if (password.value !== confirmPassword.value) {
-    match.style.color = "red";
-    match.textContent = "* Passwords do not match";
-  }
-  else if ( password === confirmPassword){
-    match.textContent = ""
-  }
-}
+  let error = document.querySelector("#error");
 
-passwordMatch();
+  if (password == "" && confirmPassword == "") {
+    error.innerHTML = "*passwords do not match";
+  } else if (password === confirmPassword) {
+    error.innerHTML = "";
+  }
+});
